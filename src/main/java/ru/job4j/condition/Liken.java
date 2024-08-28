dc906bae04d7b0d8eb2c5fd9f8fbf73e8fc5d06c
AnnaAhmedova <paradisesummer30@gmail.com>
2024-08-28 13:15:44 +0400

    0. Операции сравнения [#235631 #510788]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -1,16 +1,17 @@
 package ru.job4j.condition;
 
 public class Liken {
-
     public static void main(String[] args) {
         int first = 10;
         int second = 9;
 
-        boolean result = true;
-                System.out.println(result);
+        boolean result = first > second;
+        System.out.println(result);
 
-        result = false;
-                System.out.println(result);
-                System.out.println(result);
+        result = first < second;
+        System.out.println(result);
+
+        result = first == second;
+        System.out.println(result);
     }
 }
