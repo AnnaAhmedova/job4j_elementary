76a4b4f537a1c523fe3a1ad77a2c8b442fffc590
AnnaAhmedova <paradisesummer30@gmail.com>
2024-11-15 16:28:45 +0400

    6.4.3. Слово заканчивается на ... [#156317]

---
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] postfix) {
+        boolean result = true;
+        for (int index = 0; index <= postfix.length - 1; index++) {
+            if (postfix[index] <= word[index]) {
+                return false;
+            }
+        }
+        return result;
+    }
+}
